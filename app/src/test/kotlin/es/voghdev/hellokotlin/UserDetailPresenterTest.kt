718cc3d6f3b58496bc110837d00b67c6e47367fa
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-04-05 12:50:03 +0200

    Trying to Test a method that contains a coroutine

---
+++ app/src/test/kotlin/es/voghdev/hellokotlin/UserDetailPresenterTest.kt
@@ -0,0 +1,42 @@
+package es.voghdev.hellokotlin
+
+import android.content.Context
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+class UserDetailPresenterTest {
+    @Mock
+    lateinit var mockUserRepository: UserRepository
+
+    @Mock
+    lateinit var mockContext: Context
+
+    @Mock
+    lateinit var mockView: UserDetailPresenter.MVPView
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun `should request a List of users on start`() = runBlocking<Unit> {
+        val presenter = UserDetailPresenter(mockContext, mockUserRepository)
+
+        assertNotNull(presenter)
+
+        presenter.view = mockView
+        presenter.initialize()
+
+        verify(mockUserRepository, times(1))?.getUsers()
+    }
+
+    private fun waitForAsyncBlocksToFinish() {
+        Thread.sleep(30)
+    }
+}
