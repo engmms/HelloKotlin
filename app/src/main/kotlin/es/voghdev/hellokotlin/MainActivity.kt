678b42caf6eb6e5c7a5466453495fbbe46fef79f
Olmo Gallegos <olmo@appandweb.es>
2018-02-08 11:10:51 +0100

    Add test for blocking code

--- app/src/main/kotlin/es/voghdev/hellokotlin/MainActivity.kt
+++ app/src/main/kotlin/es/voghdev/hellokotlin/MainActivity.kt
@@ -15,15 +15,12 @@
  */
 package es.voghdev.hellokotlin
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.DisplayMetrics
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,29 +29,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener {
-            toast("You wrote: ${editText1.text}")
+            runBlocking {
+                async(CommonPool) {
+                    Thread.sleep(5000L)
 
-            doAsync() {
-                Thread.sleep(2500)
-                uiThread {
-                    val width: Int = screenWidth()
-                    val height: Int = screenHeight()
-
-                    toast("Screen size is: $width x $height")
-                }
+                    runOnUiThread { textView1.text = "Finally! I've been blocked for 5s :-(" }
+                }.await()
             }
         }
-    }
-
-    fun Context.screenWidth(): Int {
-        val displayMetrics: DisplayMetrics = DisplayMetrics()
-        (this as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
-        return displayMetrics.widthPixels
-    }
-
-    fun Context.screenHeight(): Int {
-        val displayMetrics: DisplayMetrics = DisplayMetrics()
-        (this as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
-        return displayMetrics.heightPixels
     }
 }
